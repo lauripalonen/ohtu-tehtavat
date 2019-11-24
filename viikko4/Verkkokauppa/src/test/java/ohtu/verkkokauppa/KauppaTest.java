4f1f8767b6e7eb2fab4224f4ef59c6b5533ee2e5
lauripalonen <palonen.lauri@gmail.com>
2019-11-24 21:19:28 +0200

    do task 4

--- viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -101,4 +101,51 @@ public class KauppaTest {
         verify(pankki).tilisiirto("pekka", 42, "12345", "33333-44455", 5);
     }
 
+    @Test
+    public void metodiAloitaAsiointiNollaaEdellisenOstoksenTiedot(){
+        when(viite.uusi()).thenReturn(42).thenReturn(24);
+
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.saldo(2)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "juusto", 3));
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.tilimaksu("pekka", "12345");
+
+        verify(pankki).tilisiirto("pekka", 42, "12345", "33333-44455", 5);
+        k.aloitaAsiointi();
+        k.lisaaKoriin(2);
+        k.tilimaksu("pasi", "54321");
+
+        verify(pankki).tilisiirto("pasi", 24, "54321", "33333-44455", 3);
+    }
+
+    @Test
+    public void pyydetaanUusiViiteJokaiseenMaksuun() {
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.tilimaksu("pekka", "12345");
+
+        verify(viite, times(1)).uusi();
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(2);
+        k.tilimaksu("pasi", "54321");
+
+        verify(viite, times(2)).uusi();
+    }
+
+    @Test
+    public void metodiPoistaKoristaPalauttaaTuotteenVarastoon() {
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.poistaKorista(1);
+
+        verify(varasto, times(1)).palautaVarastoon(varasto.haeTuote(1));
+    }
+
 }
