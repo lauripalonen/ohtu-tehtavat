262091161f8b0fe7669d6f032c7dab111f602f3f
lauripalonen <palonen.lauri@gmail.com>
2019-12-02 11:56:07 +0200

    do tasks 4 and 5

---
+++ viikko5/LaskinFX8/src/main/java/laskin/Summa.java
@@ -0,0 +1,55 @@
+package laskin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Summa extends Komento {
+    private int tulosMuisti;
+
+    public Summa(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus){
+        super(tuloskentta, syotekentta, nollaa, undo, sovellus);
+    }
+
+    @Override
+    public void suorita() {
+        tulosMuisti = Integer.parseInt(tuloskentta.getText());
+
+        int arvo = 0;
+
+        try {
+            arvo = Integer.parseInt(syotekentta.getText());
+        } catch (Exception e) {
+        }
+
+        sovellus.plus(arvo);
+
+        paivitaNakyma(sovellus.tulos());
+
+    }
+
+    @Override
+    public void peru() {
+        syotekentta.setText("");
+        tuloskentta.setText(Integer.toString(tulosMuisti));
+
+        if (tulosMuisti == 0){
+            nollaa.disableProperty().set(true);
+        } else {
+            nollaa.disableProperty().set(false);
+        }
+        undo.disableProperty().set(false);
+    }
+
+    private void paivitaNakyma(int laskunTulos){
+        syotekentta.setText("");
+        tuloskentta.setText("" + laskunTulos);
+
+        if ( laskunTulos==0) {
+            nollaa.disableProperty().set(true);
+        } else {
+            nollaa.disableProperty().set(false);
+        }
+        undo.disableProperty().set(false);
+    }
+
+}
